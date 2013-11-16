9df5b7158be97d6f83a6cb7f8b45bf4c868d4b49
Sonny Theo Tumbur Manurung <sonthehottest@gmail.com>
2013-11-16 08:57:29 +0700

    Removed Ads in mangafox.me

--- data/remove-ad.js
+++ data/remove-ad.js
@@ -1,5 +1,13 @@
+console.log('masuk removeAD');
 self.port.on("findElmt", function(ban){
-    console.log('ban: '+ban);
+/*    membaca file daftar URL yang harus diantisipasi*/
+    var adList = new Array();
+    adList.push("http://go.game321.com/mgf_gv.htm");
+    adList.push("http://www.googleadservices.com/");
+    adList.push("http://googleads.g.doubleclick.net/");
+    adList.push("http://es.mangahere.com");
+    adList.push("http://the3dgame.com/most-popular/?nxc=MFBANNER");
+    adList.push("http://funnymama.com");
     
     var all = document.getElementsByTagName("*");
     var max = all.length;
@@ -7,35 +15,32 @@ self.port.on("findElmt", function(ban){
     
 //    penghitungan all.length hanya dilakukan sekali
     for(var i=0; i<max; i++)
-    {
-/*        console.log('elemen: '+all[i].innerHTML);*/
-//        console.log('attribute length: '+all[i].attributes.length);
-        
+    {        
         if(all[i].attributes.length != 0)   
 		{
             var attrs=all[i].attributes;
             var l=attrs.length;
             var attr;
 //            ambil seluruh atribut element
-            var nodes=[], values=[];
             for (var j=0; j<l; j++)
             {
                 attr = attrs.item(j);
                 if(attr.nodeName != null)
                 {
-                    nodes.push(attr.nodeName);
-//                    console.log('NodeName: '+attr.nodeName);                  
                     if(attr.nodeValue != null)
-                    {
-                        values.push(attr.nodeValue);
-                        
-                        if(attr.nodeName == 'href') console.log(attr.nodeName+'='+attr.nodeValue);
-                        
-//                        contoh satu link advertisement
-                        if(attr.nodeValue == ban)
+                    {                     
+//                        hilangkan tiap ad dari adList
+                        for(var k=0; k<adList.length; k++)
                         {
-                            console.log('cocok');
-                            all[i].style.visibility = 'hidden';
+                            if(attr.nodeValue.indexOf(adList[k]) !== -1)
+                            {
+                                console.log('cocok '+adList[k]);
+//                                all[i].style.visibility = 'hidden';
+                                all[i].style.display = 'none';
+//                                all[i].parentNode.removeChild(all[i]);
+//                                all[i].parentNode.style.display = 'none';
+                                break;
+                            }
                         }
                     }
                 }
